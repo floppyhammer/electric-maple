bb3b8466bf0f347fd90a687e57fa19a9187477bc
floppyhammer <tannhauser_chen@outlook.com>
2025-10-31 09:52:07 +0800

    client/em_sample: Remove unused includes.

--- client/src/em/em_sample.h
+++ client/src/em/em_sample.h
@@ -1,4 +1,4 @@
-// Copyright 2020-2023, Collabora, Ltd.
+// Copyright 2020-2025, Collabora, Ltd.
 // Copyright 2023, Pluto VR, Inc.
 //
 // SPDX-License-Identifier: BSL-1.0
@@ -10,18 +10,14 @@
  * @author Moshi Turner <moses@collabora.com>
  * @author Jakob Bornecrantz <jakob@collabora.com>
  * @author Christoph Haag <christoph.haag@collabora.com>
+ * @author Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
  * @ingroup xrt_fs_em
  */
 
 #pragma once
 
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <GLES3/gl3.h>
 #include <openxr/openxr.h>
-#include <openxr/openxr_platform.h>
-#include <stdbool.h>
-#include <string.h>
-#include <sys/socket.h>
 
 struct em_sample
 {
